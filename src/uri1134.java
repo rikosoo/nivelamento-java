45cdc2499f3079e1a38191fc090112ba6ca537d7
Henrique Stefanutti <52385414+rikosoo@users.noreply.github.com>
2024-02-15 13:36:56 -0300

    Update uri1134.java
    Apenas ajustando texto para facilitar a visualização

--- src/uri1134.java
+++ src/uri1134.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
 
-public class uri1134 {
+public class CombustivelAbastecido {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
 
-	public static void main(String[] args) {
+        System.out.println("Digite 1 para soma alcool");
+        System.out.println("Digite 2 para soma gasolina");
+        System.out.println("Digite 3 para soma Diesel");
+        System.out.println("Digite 4 para o relatório");
+        int alcool =0;
+        int gasolina=0;
+        int diesel=0;
 
-		Scanner sc = new Scanner(System.in);
-		
-		int alcool = 0;
-		int gasolina = 0;
-		int diesel = 0;
-		
-		int tipo = sc.nextInt();
-		
-		while (tipo != 4) {
-			if (tipo == 1) {
-				alcool = alcool + 1;
-			}
-			else if (tipo == 2) {
-				gasolina = gasolina + 1;
-			}
-			else if (tipo == 3) {
-				diesel = diesel + 1;
-			}
-			
-			tipo = sc.nextInt();
-		}
+        int tipo = sc.nextInt();
 
-		System.out.println("MUITO OBRIGADO");
-		System.out.println("Alcool: " + alcool);
-		System.out.println("Gasolina: " + gasolina);
-		System.out.println("Diesel: " + diesel);
-		
-		sc.close();
-	}
+        while (tipo!=4){
+            if (tipo==1){
+                alcool = alcool + 1;
+            } else if (tipo==2) {
+                gasolina=gasolina+1;
+            } else if (tipo==3) {
+                diesel=diesel+1;
+            } else if (tipo >4) {
+                System.out.println("Favor, inserir um codigo valido");
+            }
+            System.out.println("Qual o próximo abastecimento");
+            tipo = sc.nextInt();
+
+        }
+
+        System.out.println("Muito Obrigado");
+        System.out.println("Alcool: " +alcool);
+        System.out.println("Gasolina :" + gasolina);
+        System.out.println("Diesel: "+diesel);
+        sc.close();
+
+    }
 }
